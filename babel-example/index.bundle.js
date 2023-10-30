eea7906c8e780e00d09c48416751899b31f6e51f
1uokun <luokun199503@qq.com>
2023-10-30 15:25:56 +0800

    feat: 创建babel-example(babel/cli打包)

---
+++ babel-example/index.bundle.js
@@ -0,0 +1,14 @@
+"use strict";
+
+var _interopRequireDefault = require("@babel/runtime-corejs3/helpers/interopRequireDefault");
+var _createClass2 = _interopRequireDefault(require("@babel/runtime-corejs3/helpers/createClass"));
+var _classCallCheck2 = _interopRequireDefault(require("@babel/runtime-corejs3/helpers/classCallCheck"));
+// import "core-js/stable";
+var A = /*#__PURE__*/(0, _createClass2.default)(function A() {
+  (0, _classCallCheck2.default)(this, A);
+  console.log('Hello world!');
+}); // async function a(){
+//     console.log("a");
+// }
+// a()
+// new A()
---
+++ babel-example/index.js
@@ -0,0 +1,15 @@
+// import "core-js/stable";
+
+class A {
+    constructor() {
+        console.log('Hello world!');
+    }
+}
+
+// async function a(){
+//     console.log("a");
+// }
+
+// a()
+
+// new A()
