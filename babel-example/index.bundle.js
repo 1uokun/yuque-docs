888206954f46df3b41058abeabdc24505e2d3541
1uokun <luokun199503@qq.com>
2023-11-03 15:11:03 +0800

    feat: 自制babel-plugin-import.js

--- babel-example/index.bundle.js
+++ babel-example/index.bundle.js
@@ -3,10 +3,12 @@
 var _interopRequireDefault = require("@babel/runtime-corejs3/helpers/interopRequireDefault");
 var _createClass2 = _interopRequireDefault(require("@babel/runtime-corejs3/helpers/createClass"));
 var _classCallCheck2 = _interopRequireDefault(require("@babel/runtime-corejs3/helpers/classCallCheck"));
+var _button = _interopRequireDefault(require("antd/lib/button"));
 // import "core-js/stable";
 var A = /*#__PURE__*/(0, _createClass2.default)(function A() {
   (0, _classCallCheck2.default)(this, A);
   console.log('Hello world!');
+  console.log(_button.default);
 }); // async function a(){
 //     console.log("a");
 // }
--- babel-example/index.js
+++ babel-example/index.js
@@ -1,8 +1,10 @@
 // import "core-js/stable";
+import {Button} from 'antd'
 
 class A {
     constructor() {
         console.log('Hello world!');
+        console.log(Button)
     }
 }
 
