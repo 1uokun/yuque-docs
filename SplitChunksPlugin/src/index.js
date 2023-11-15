84189285272ce9ae26cafd548e2d915989a2cdb1
罗坤 <luokun@dxy.cn>
2023-11-15 16:30:39 +0800

    feat: SplitChunksPlugin分包插件

---
+++ SplitChunksPlugin/src/index.js
@@ -0,0 +1,12 @@
+import React from "react";
+import ReactDOM from "react-dom";
+import common from "./common";
+
+console.log(1, React, ReactDOM);
+console.log(2, common);
+
+setTimeout(() => {
+  import("./async-module").then(({ default: common }) => {
+    console.log(3, common);
+  });
+}, 2000);
